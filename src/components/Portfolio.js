09f2a3d174323e87a4328728d0221685d22cf2e8
Charlie Cluff <charlie_cluff@yahoo.com>
2021-06-06 17:35:25 -0700

    Initial commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1 style={{fontSize: 30}}>Portfolio</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          <h1>Music Production</h1>
+
+          {
+            resumeData.musicPortfolio && resumeData.musicPortfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href={item.url} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          <h1>Sound Design</h1>
+
+          {
+            resumeData.sdPortfolio && resumeData.sdPortfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                  <a href={item.url} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+          
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          <h1>Broadcast</h1>
+
+          {
+            resumeData.radioPortfolio && resumeData.radioPortfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                  <a href={item.url} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          <h1>Audio Programming</h1>
+
+          {
+            resumeData.progPortfolio && resumeData.progPortfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                  <a href={item.url} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          <h1>App Development</h1>
+
+          {
+            resumeData.devPortfolio && resumeData.devPortfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                  <a href={item.url} target="_blank">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,224 @@
+let resumeData = {
+    "name": "Charlie Cluff",
+    "role": " ",
+    "linkedinId":"charlie-david-cluff",
+    "soundcloudid": "Your soundcloudid",
+    "roleDescription": "I specialize in in all things Music Production, Sound Design, Audio Programming, and Front End App development.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/charlie-cluff-a507249a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/thecluff",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"soundcloud",
+          "url":"http://soundcloud.com/thecluff",
+          "className":"fa fa-music"
+        }
+      ],
+    "aboutme":"I'm a San Francisco Bay Area based Augio Engineer sepcializing in Music Production, Sound Design, and Audio Programming. Additionally, I am experieneced in Web & Mobile Application Development.",
+    "address":"San Jose, CA",
+    "website":"https://thecluff.com/",
+    "education":[
+      {
+        "UniversityName":"Cogswell Polytechnical College",
+        "specialization":"B.S.  Digital Audio Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Universal Media Access",
+        "specialization":"Audio Producer | Mobile App Developer",
+        "MonthOfLeaving":"Feb 2016",
+        "YearOfLeaving":" - Present",
+        "Achievements":"Audio Producer and Board Operator for KSJO, 92.3FM",
+        "Achievements2":" Designed and developed Bolly 92.3FM Mobile App 2.0 with React Native",
+        "Achievements3":"Engineered live studio sessions for NPR, WBUR, and BBC"
+      },
+      {
+        "CompanyName":"HODL Corp.",
+        "specialization":"Music Producer",
+        "MonthOfLeaving":"Sep 2017",
+        "YearOfLeaving":"Present",
+        "Achievements":"Some Achievements",
+        "Achievements":"Music Producer for teamHODL, a cryptocurrency rap group aimed at bringing awareness to the Blockchain space through music and art.",
+        "Achievements2":" Performed at NEO DevCon 1 - San Francisco, 2018",
+        "Achievements3":"Nominated for Best Crypto Musician - Crypto Influence Summit, NYC, 2018"
+      },
+      {
+        "CompanyName":"MediaWorks",
+        "specialization":"Sound Designer",
+        "MonthOfLeaving":"May 2020",
+        "YearOfLeaving":"April 2020",
+        "Achievements":"Designed SFX for Women In Games International logo reveal",
+        "Achievements2":"Students in the MediaWorks course produce impressive, industry-quality creative work for businesses in Silicon Valley and beyond.",
+      },
+      {
+        "CompanyName":"Make Play Date, Inc.",
+        "specialization":"Mobile App Developer",
+        "MonthOfLeaving":"April 2018",
+        "YearOfLeaving":"March 2019",
+        "Achievements":"Designed playdate 2.0 UI and brought designs into reality with React Native",
+        "Achievements2":"Developed features that had +90% adoption rates, doubled user activity, and reduced churn by 50%, resulting in 5.8k active users and 6.7k social media followers"
+      },
+      {
+        "CompanyName":"Braille Skateboarding",
+        "specialization":"Video Editor",
+        "MonthOfLeaving":"April 2017",
+        "YearOfLeaving":"April 2018",
+        "Achievements":"Video editor for Braille Skateboarding, the largest skateboarding channel on YouTube."
+      },
+      {
+        "CompanyName":"SkyCog, Inc.",
+        "specialization":"Web Development Internship",
+        "MonthOfLeaving":"June 2008",
+        "YearOfLeaving":"September 2012",
+        "Achievements":"Worked with HTML, CSS, ASP/ASPX, and SQL",
+        "Achievements2":"Performed front end website and internal dashboard UI maintenance, DNS maintenance, and data backup",
+        "Achievements3":"(Previously Kopavi Systems, Inc.)",
+      },
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Music Production | Logic Pro X, Pro Tools, Reason, Ableton, Audition "
+      },
+      {
+        "skillname":"Sound Design"
+      },
+      {
+        "skillname":"Interactive Audio | Wwise, FMOD"
+      },
+      {
+        "skillname":"Digital Signal Processing | Swift, C++, JS "
+      },
+      {
+        "skillname":"Video Production | Final Cut Pro, Premiere, Davinci Resolve, After Effects, Cinema4D"
+      },
+      {
+        "skillname":"Front End Development | JS (React Native), Swift, CSS/HTML"
+      },
+    ],
+    "musicPortfolio":[
+      {
+        "name":"Professor Ali - XFactor (2015)",
+        "description":"Session Turntablist (Mixed by Deegan Mack Adams)",
+        "imgurl":"https://i1.sndcdn.com/artworks-000102166224-6a93cp-t500x500.jpg",
+        "url":"https://music.apple.com/gh/album/xfactor/955634683"
+      },
+      {
+        "name":"Kaly Jay - The Sky Is Purple LP (2016)",
+        "description":"Composer, Producer, Engineer",
+        "imgurl":"https://m.media-amazon.com/images/I/71sLbxdT+nL._SS500_.jpg",
+        "url":"https://music.apple.com/nz/album/the-sky-is-purple/1107745632"
+      },
+      {
+        "name":"Mikos Da Gawd & Charlie Cluff - Brand New (2018)",
+        "description":"Co-Producer",  
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623024356/avatars-TqFSJdprTeLhNeu3-Oy5GAQ-t500x500_kww5mh.jpg",
+        "url":"https://soundcloud.com/smilehighclubcommunity/brandnew"
+      },
+      {
+        "name":"teamHODL - 2.0 EP (2021)",
+        "description":"Composer, Producer, Engineer",
+        "imgurl":"https://is4-ssl.mzstatic.com/image/thumb/Music114/v4/20/e7/0f/20e70f59-557f-1fa0-6bf1-9cedc14ad25a/artwork.jpg/1200x1200bf-60.jpg",
+        "url":"https://music.apple.com/us/album/2-0-ep/1554371126"
+      }
+    ],
+
+    "sdPortfolio":[
+      {
+        "name":"Women in Games Int'l Logo Intro",
+        "description":"Sound Design",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623017020/wigi_lnvyth.jpg",
+        "url":"https://www.twitch.tv/videos/1036305497?t=00h00m49s"
+      },
+      {
+        "name":"Playdate",
+        "description":"Notification Alert & UI Sound Design",  
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623020073/mpd_ia7mag.png",
+        "url":"https://makeplaydate.com"
+      }
+    ],
+    "radioPortfolio":[
+      {
+        "name":"WBUR | Gene-Editing Gets A Go-Ahead (February 2017)",
+        "description":"Audio Engineer",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623016946/onpoint_ezvzsc.png",
+        "url":"https://www.wbur.org/onpoint/2017/02/22/gene-editing-crispr"
+      },
+      {
+        "name":"BBC | Being Open About Breast Cancer (May 2017)",
+        "description":"Audio Engineer",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623016944/bbc_ij4wc3.png",
+        "url":"https://www.bbc.co.uk/programmes/p052yb20"
+      },
+      {
+        "name":"NPR | How I Built This Ft. Gary Kremen (October 2016) [Unreleased]",
+        "description":"Audio Engineer",  
+        "imgurl":"https://media.npr.org/assets/img/2018/08/03/npr_hibt_podcasttile_sq-98320b282169a8cea04a406530e6e7b957665b3f.jpg?s=500",
+        "url":"https://www.npr.org/podcasts/510313/how-i-built-this"
+      },
+    ],
+    "progPortfolio":[
+      // {
+      //   "name":"Fluid Wavetable Synthesizer Virtual Instrument",
+      //   "description":"[Coming Soon]",
+      //   "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623021324/concept1_stwtjn.png",
+      //   "url":"https://github.com/thecluff"
+      // },
+      {
+        "name":"Multi Effects Audio Processing Tool (2019)",
+        "description":"DSP written in Swift",  
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623022039/DAT331_AudioEffectsProcessing_hbhgk8.png",
+        "url":"https://github.com/thecluff/swiftaudioprogramming"
+      },
+      {
+        "name":"PureData Sound Synthesis Patch Library (2014)",
+        "description":"mobileapp",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623022291/puredata_efruyx.png",
+        "url":"https://github.com/thecluff"
+      }
+    ],
+    "devPortfolio":[
+      {
+        "name":"Bolly 92.3 FM Mobile App",
+        "description":"Designer & Developer",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623020669/bollyIcon_fbryhe.png",
+        "url":"https://bolly923fm.com/"
+      },
+      {
+        "name":"Bamboozled!",
+        "description":"Front End Developer",
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623020504/bamb_copy_id6ess.jpg",
+        "url":"https://bamboozledapp.com/"
+      },
+      {
+        "name":"Playdate",
+        "description":"Front End Developer",  
+        "imgurl":"https://res.cloudinary.com/teamhodl/image/upload/v1623020073/mpd_ia7mag.png",
+        "url":"https://makeplaydate.com"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
